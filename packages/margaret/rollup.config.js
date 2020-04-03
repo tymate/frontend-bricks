cc2256e26481934d595c0c019745c8068c0e71fb
Henri Michel <henri@tymate.com>
2020-04-03 14:46:08 +0200

    feat: Add terser Rollup plugin

--- packages/margaret/rollup.config.js
+++ packages/margaret/rollup.config.js
@@ -1,8 +1,10 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import strip from '@rollup/plugin-strip';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.js',
@@ -12,11 +14,15 @@ export default {
   },
   plugins: [
     peerDepsExternal(),
-    babel(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
     commonjs(),
     resolve(),
     postcss({
       plugins: [],
     }),
+    strip(),
+    terser(),
   ],
 };
