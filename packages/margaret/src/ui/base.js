230d63a360c1274a653bf055964ed0630722e69c
Henri Michel <henri@tymate.com>
2020-05-29 15:47:53 +0200

    feat: Add responsive alignX/alignY props handling

--- packages/margaret/src/ui/base.js
+++ packages/margaret/src/ui/base.js
@@ -180,9 +180,21 @@ export const Stack = styled(Box)`
           breakpoint => media[breakpoint]`
             flex-direction: ${({ direction }) => direction[breakpoint]};
             align-items: ${({ alignX, alignY }) =>
-              direction[breakpoint] === 'row' ? alignY : alignX};
+              direction[breakpoint] === 'row'
+                ? typeof alignY === 'object'
+                  ? alignY[breakpoint]
+                  : alignY
+                : typeof alignX === 'object'
+                ? alignX[breakpoint]
+                : alignX};
             justify-content: ${({ alignX, alignY }) =>
-              direction[breakpoint] === 'row' ? alignX : alignY};
+              direction[breakpoint] === 'row'
+                ? typeof alignX === 'object'
+                  ? alignX[breakpoint]
+                  : alignX
+                : typeof alignY === 'object'
+                ? alignY[breakpoint]
+                : alignY};
 
             > * + * {
               ${({ theme, gutterSize }) =>
@@ -200,6 +212,11 @@ export const Stack = styled(Box)`
                   direction:
                     direction[breakpoint] === 'column' ? 'top' : 'left',
                 })};
+                
+              align-items: ${({ alignX, alignY }) =>
+                direction[breakpoint] === 'column' ? alignX : alignY};
+              justify-content: ${({ alignX, alignY }) =>
+                direction[breakpoint] === 'column' ? alignY : alignX};
             }
           `,
         )}
